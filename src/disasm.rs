fb6303483c6644f78b04181f187fa973ffd63246
bugzmanov <bugzmanov@gmail.com>
2020-05-20 21:05:59 -0400

    disasm output improved

--- src/disasm.rs
+++ src/disasm.rs
@@ -2,10 +2,13 @@ use crate::cpu::opscode;
 use byteorder::{ByteOrder, LittleEndian};
 use std::collections::HashMap;
 use std::cmp::min;
+use crate::cpu::cpu::AddressingMode;
+
 
 pub struct Disasm {
-    program: Vec<String>,
-    ops_index_map: HashMap<u16, usize>,
+    pub program: Vec<String>,
+    pub hex_dump: Vec<Vec<u8>>,
+    pub ops_index_map: HashMap<u16, usize>,
 }
 
 
@@ -17,16 +20,42 @@ impl Disasm {
         let mut begin = start;
         let mut asm = Vec::new();
         let mut mapping: HashMap<u16, usize> = HashMap::new();
-        while begin < program.len() {
+        let mut hex_dump: Vec<Vec<u8>> = Vec::new();
+        while begin < program.len() {  //todo: should be another condition as well
             let code = &program[begin];
             let ops = opscodes.get(code).unwrap();
     
             let tmp = match ops.len {
-                2 => format!("#${:02x}", program[begin + 1]),
-                3 => format!(
-                    "#{:x}",
-                    LittleEndian::read_u16(&program[begin + 1 as usize..])
-                ),
+                1 => {
+                    hex_dump.push(vec!(*code));
+                    String::from("")
+                },
+                2 => {
+                    let address: u8 = program[begin + 1];
+                    hex_dump.push(vec!(*code, address));
+                    match ops.mode {
+                        AddressingMode::Immediate => format!("#${:02x}", address),
+                        AddressingMode::ZeroPage => format!("${:02x}", address),
+                        AddressingMode::ZeroPage_X => format!("${:02x},X", address), 
+                        AddressingMode::ZeroPage_Y => format!("${:02x},Y", address), 
+                        AddressingMode::Indirect_X => format!("(${:02x},X)", address), 
+                        AddressingMode::Indirect_Y => format!("(${:02x}),Y", address),
+                        AddressingMode::NoneAddressing => {
+                            // assuming local jumps: BNE, BVS, etc.... todo: check ?
+                            let address: usize = (begin +2).wrapping_add((address as i8) as usize);
+                            format!("${:04x}", address)
+                        },
+                        
+                        _ => panic!("unexpected addressing mode {:?} has size ops-len 2", ops.mode),
+                    }
+                    
+                },
+                3 => {
+                    hex_dump.push(vec!(*code, program[begin+1], program[begin+2]));
+                    format!(
+                    "${:04x}",
+                    LittleEndian::read_u16(&program[begin + 1 as usize..]))
+                },
                 _ => String::from(""),
             };
     
@@ -37,6 +66,7 @@ impl Disasm {
         Disasm {
             program: asm,
             ops_index_map: mapping,
+            hex_dump: hex_dump
         }    
     }
 
@@ -53,6 +83,7 @@ impl Disasm {
         }
     }
 }
+
 pub fn disasm(program: &[u8], start: usize) -> Vec<String> {
     let ref opscodes: HashMap<u8, &'static opscode::OpsCode> = *opscode::OPSCODES_MAP;
 
@@ -88,6 +119,7 @@ mod test {
         let asm = Disasm::new(&CPU::transform("a2 08 ca"), 0);
         let result= vec!("0000: LDX #$08", "0002: DEX");
         assert_eq!(asm.program, result);
+        assert_eq!(asm.hex_dump, vec!(vec!(0xa2, 0x08), vec!(0xca)));
         assert_eq!(asm.ops_index_map.get(&0), Some(&0));
         assert_eq!(asm.ops_index_map.get(&2), Some(&1));
     }
@@ -95,22 +127,23 @@ mod test {
     #[test]
     fn test_slice() {
         let asm = Disasm::new(&CPU::transform("a2 08 ca c8 e0 03 d0 fa 00 a2 08 ca c8 e0 03 d0 fa 00"), 0);
-        let result= vec!("0000: LDX #$08", "0002: DEX", "0003: INY", "0004: CPX #$03", "0006: BNE #$fa", "0008: BRK", "0009: LDX #$08", "000b: DEX", "000c: INY", "000d: CPX #$03", "000f: BNE #$fa", "0011: BRK");
+        let result= vec!("0000: LDX #$08", "0002: DEX", "0003: INY", "0004: CPX #$03", "0006: BNE $0002", "0008: BRK", "0009: LDX #$08", "000b: DEX", "000c: INY", "000d: CPX #$03", "000f: BNE $000b", "0011: BRK");
         assert_eq!(asm.program, result);
         let (slice, idx) = asm.slice(0004);
         assert_eq!(3, idx);
         assert_eq!(10, slice.len());
-        assert_eq!(slice, &["0000: LDX #$08", "0002: DEX", "0003: INY", "0004: CPX #$03", "0006: BNE #$fa", "0008: BRK", "0009: LDX #$08", "000b: DEX", "000c: INY","000d: CPX #$03"]);
+        assert_eq!(slice, &result[0..10]);
     }
 
     #[test]
     fn test_slice_end_of_program() {
         let asm = Disasm::new(&CPU::transform("a2 08 ca c8 e0 03 d0 fa 00 a2 08 ca c8 e0 03 d0 fa 00"), 0);
-        let result= vec!("0000: LDX #$08", "0002: DEX", "0003: INY", "0004: CPX #$03", "0006: BNE #$fa", "0008: BRK", "0009: LDX #$08", "000b: DEX", "000c: INY", "000d: CPX #$03", "000f: BNE #$fa", "0011: BRK");
+        let result= vec!("0000: LDX #$08", "0002: DEX", "0003: INY", "0004: CPX #$03", "0006: BNE $0002", "0008: BRK", "0009: LDX #$08", "000b: DEX", "000c: INY", "000d: CPX #$03", "000f: BNE $000b", "0011: BRK");
         assert_eq!(asm.program, result);
         let (slice, idx) = asm.slice(0x000f);
         assert_eq!(8, idx);
         assert_eq!(10, slice.len());
-        assert_eq!(slice, &["0003: INY", "0004: CPX #$03", "0006: BNE #$fa", "0008: BRK", "0009: LDX #$08", "000b: DEX", "000c: INY", "000d: CPX #$03", "000f: BNE #$fa", "0011: BRK"]);
+        assert_eq!(slice, &result[2..12]);
     }
+
 }
