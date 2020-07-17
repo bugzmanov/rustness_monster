3f7735db20b69f15d65ae4322ef59ef7df14866c
bugzmanov <bugzmanov@gmail.com>
2020-07-17 14:46:51 -0400

    and another broken test...

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -58,7 +58,7 @@ mod test {
     fn test_strobe_mode() {
         let mut joypad = Joypad::new();
         joypad.write(1);
-        joypad.set_button_pressed_status(JoypadButton::RIGHT, true);
+        joypad.set_button_pressed_status(JoypadButton::BUTTON_A, true);
         for _x in 0..10 {
             assert_eq!(joypad.read(), 1);
         }
@@ -75,15 +75,15 @@ mod test {
         joypad.set_button_pressed_status(JoypadButton::BUTTON_B, true);
 
         for _ in 0..=1 {
-            assert_eq!(joypad.read(), 1);
-            assert_eq!(joypad.read(), 1);
-            assert_eq!(joypad.read(), 0);
-            assert_eq!(joypad.read(), 0);
             assert_eq!(joypad.read(), 0);
             assert_eq!(joypad.read(), 1);
             assert_eq!(joypad.read(), 1);
             assert_eq!(joypad.read(), 0);
-
+            assert_eq!(joypad.read(), 0);
+            assert_eq!(joypad.read(), 0);
+            assert_eq!(joypad.read(), 1);
+            assert_eq!(joypad.read(), 1);            
+            
             for _x in 0..10 {
                 assert_eq!(joypad.read(), 1);
             }
